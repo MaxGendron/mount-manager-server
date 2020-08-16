65e0b78204e2da5a7d7974359b80a0a7f00a5333
Maxime <gendron.maxime1@gmail.com>
2020-08-16 12:02:49 -0400

    User role

--- src/users/models/dtos/new-user.dto.ts
+++ src/users/models/dtos/new-user.dto.ts
@@ -1,13 +1,18 @@
-import { IsNotEmpty, Matches } from 'class-validator';
+import { IsNotEmpty, Matches, IsEnum, IsOptional } from 'class-validator';
+import { UserRoleEnum } from '../enum/user-role.enum';
 
 export class NewUserDto {
   @IsNotEmpty()
   username: string;
-  
+
   @IsNotEmpty()
   @Matches(new RegExp('^[A-Z0-9._%+-]+@(?:[A-Z0-9-]+.)+[A-Z]{2,}$', 'i'))
   email: string;
 
   @IsNotEmpty()
   password: string;
+
+  @IsEnum(UserRoleEnum)
+  @IsOptional()
+  role: UserRoleEnum;
 }
